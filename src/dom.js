e9cfc2bdd6b4b3e33df51c732566270983ae5abe
Sigolletes <juditreverter87@gmail.com>
2022-11-24 17:05:46 +0100

    Set up project files and configuration

---
+++ src/dom.js
@@ -0,0 +1,5 @@
+// UI utilities:
+  // view all projects
+  // view all todos of a project (just title and dueDate), different colors for priorities
+  // expand a single todo / edit details
+  // delete a todo
---
+++ src/logic.js
@@ -0,0 +1,8 @@
+// factory or class for todo creation
+  // properties: title, description, dueDate, priority; optional: notes, checklist
+  // choose project
+  // use date-fns for dates manipulating
+
+// project creation for store related todos and a global one
+
+// use localStorage for storage data
