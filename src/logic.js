c807b7943e65cde6735997f753640cd127ce4f7a
Sigolletes <juditreverter87@gmail.com>
2022-11-24 18:40:15 +0100

    Start todo logic

--- src/logic.js
+++ src/logic.js
@@ -3,6 +3,23 @@
   // choose project
   // use date-fns for dates manipulating
 
-// project creation for store related todos and a global one
+  // project creation for store related todos and a global one
+const projects = { 
+  allProjects: [],
+};
+
+class Todo {
+  constructor(title, description = '', dueDate = '', priority = 1, checklist = []) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checklist = checklist;
+
+    projects.allProjects.push(this);
+  }
+}
+
+
 
 // use localStorage for storage data
